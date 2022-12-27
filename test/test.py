05ab70b083254b0f7bec74c95554420efe964147
Aryaan Bhaskar <82225839+AryaanB9@users.noreply.github.com>
2022-12-27 21:29:39 +0530

    Updated test.py
    The TensorFlow Warnings and Info are now hidden.
    Performed Code Cleaning

--- test/test.py
+++ test/test.py
@@ -1,52 +1,36 @@
-import warnings
-import joblib
-# import feature
 import sys
 import numpy as np
-import pickle
 from feature import FeatureExtraction
-from feature import MODEL_PATH
+import os
+
+#** For Removing the INFO, WARNING and ERROR messages of Tensorflow
+#* 0 = all messages are logged (default behavior)
+#* 1 = INFO messages are not printed
+#* 2 = INFO and WARNING messages are not printed
+#* 3 = INFO, WARNING, and ERROR messages are not printed
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 from tensorflow import keras
 
-pickle_model = keras.models.load_model('C:/xampp/htdocs/DJ22Trial3/train/mymodel1')
-
-# file = open("D:/DevJams2022/Trial2/model.pkl","rb")
-# pickle_model = pickle.load(open("C:/xampp/htdocs/DJ22Trial3/model3.pkl","rb"))
-# filename = 'model4'
-# pickle_model = joblib.load(MODEL_PATH)
-# file.close()
+loaded_model = keras.models.load_model("../model/trainedModel")
 
 def predict(test_url):
-
     obj = FeatureExtraction(test_url)
     x = np.array(obj.getFeaturesList()).reshape(1,30) 
-
-    y_pred = pickle_model.predict(x)[0]
-
-    # 1 is safe
-    # -1 is unsafe
-
-    # y_pro_phishing = pickle_model.predict_proba(x)[0,0]
-    # y_pro_non_phishing = pickle_model.predict_proba(x)[0,1]
-    # if(y_pred ==1 ):
-    # pred = "It is {0:.2f} % safe to go ".format(y_pro_phishing*100)
-
+    y_pred = loaded_model.predict(x)[0]
     return y_pred[0]
-    # return x
-
 
 def main():
     url = sys.argv[1]
-    
+    # url = "http://phished.com/"
     prediction = predict(url)
-    # print("Prediction : "+str(prediction))
-    # features = featureExtract(url)
-    if prediction <= 1 and prediction > 0.5:
+    
+    print("Checking URL : ", url);
+    print("Safety % : "+str(prediction*100))
+    if prediction <= 1 and prediction > 0.8:
         print("SAFE")
-    elif prediction <= 0.5 and prediction > 0.1:
+    elif prediction <= 0.8 and prediction > 0.2:
         print("SUSPICIOUS")
     else:
-        print("PHISHING")
-    # print(features)
+        print("MALICIOUS")
 
 main()
